cd1db1ba0ce9cc319c8351d3ce8c94fbac231d72
lefort999 <jlefort251@gmail.com>
2025-06-28 18:42:41 +0200

    Update voila.py

--- voila.py
+++ voila.py
@@ -1,1 +1,12 @@
-echo "hello" > test
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def recherche():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))  # Port par défaut = 10000
+    app.run(host="0.0.0.0", port=port)
